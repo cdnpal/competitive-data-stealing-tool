7cfd36bb1f5403816bff737945902deed394bc32
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 06:16:36 -0700

    Update on the comment to elaborate.

--- src/main/java/com/carsdirect/cd/feeds/TrueCarProprietaryRegionalPricingData.java
+++ src/main/java/com/carsdirect/cd/feeds/TrueCarProprietaryRegionalPricingData.java
@@ -1,5 +1,9 @@
 package com.carsdirect.cd.feeds;
 
 public class TrueCarProprietaryRegionalPricingData {
-
+	/*
+	 * As mandated, we will not only take proprietary pricing data
+	 * we will basically empty the rival's database here of all 
+	 * car data, including vID codes, trim codes, make/model info,  ect..
+	 */
 }
