014c2da689cb92cdd5b90b6ba088bbe83cf4cb87
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 19:14:08 -0700

    Added some comments about the app context, need to set one up for this DB to use in the DAO methods.

--- src/main/java/com/carsdirect/cd/dao/CompetitiveDataDAO.java
+++ src/main/java/com/carsdirect/cd/dao/CompetitiveDataDAO.java
@@ -2,4 +2,12 @@ package com.carsdirect.cd.dao;
 
 public class CompetitiveDataDAO {
 
+	public CompetitiveDataDAO() {
+		/*
+		 * Initialize the Application Context here
+		 * 
+		 * CompetitiveDataContext competitiveDataContext = (CompetitiveDataContext)new AppContext("CompetitiveData");
+		 */
+	}
+	
 }
