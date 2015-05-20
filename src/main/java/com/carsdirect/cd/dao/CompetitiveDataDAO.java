868d22eaa9970447e18f359456bbc2968a1c90e9
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 19:16:24 -0700

    Comment +SessionFactory

--- src/main/java/com/carsdirect/cd/dao/CompetitiveDataDAO.java
+++ src/main/java/com/carsdirect/cd/dao/CompetitiveDataDAO.java
@@ -6,7 +6,7 @@ public class CompetitiveDataDAO {
 		/*
 		 * Initialize the Application Context here
 		 * 
-		 * CompetitiveDataContext competitiveDataContext = (CompetitiveDataContext)new AppContext("CompetitiveData");
+		 * CompetitiveDataContext competitiveDataContext = (CompetitiveDataContext)new AppContext("CompetitiveDataSessionFactory");
 		 */
 	}
 	
