32345f1ffff83317f38eef98b22322e262b18304
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 19:34:27 -0700

    Interfaces are referenced in the Dependency Injection beans.xml files but are set to instances of the Impl classes with Spring autowire.

--- src/main/java/com/carsdirect/cd/service/CompetitiveDataStealingServiceImpl.java
+++ src/main/java/com/carsdirect/cd/service/CompetitiveDataStealingServiceImpl.java
@@ -2,4 +2,8 @@ package com.carsdirect.cd.service;
 
 public class CompetitiveDataStealingServiceImpl implements CompetitiveDataStealingService {
 
+	/*
+	 * Interfaces are referenced in the Dependency Injection beans.xml files
+	 * but are set to instances of the Impl classes with Spring autowire
+	 */
 }
