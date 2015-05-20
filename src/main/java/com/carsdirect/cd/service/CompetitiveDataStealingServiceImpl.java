84ddca0411e9d4d4c57fb7ddf4302097a27518ce
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 21:08:54 -0700

    More inline notes about the HTTP anonymous proxy collection

--- src/main/java/com/carsdirect/cd/service/CompetitiveDataStealingServiceImpl.java
+++ src/main/java/com/carsdirect/cd/service/CompetitiveDataStealingServiceImpl.java
@@ -6,4 +6,26 @@ public class CompetitiveDataStealingServiceImpl implements CompetitiveDataSteali
 	 * Interfaces are referenced in the Dependency Injection beans.xml files
 	 * but are set to instances of the Impl classes with Spring autowire
 	 */
+	
+	/*
+	 * 1. All Config data managed by business pulled from 
+	 * the VendorConfigData
+	 * 
+	 * A combination of zipcodes, make, model, year
+	 * for each competitor VendorID
+	 * 
+	 * 2. All Vendor IDs (ie. ChromeTrimID for TrueCar)
+	 * was pulled for that make/model/year, using 
+	 * detection avoidance, anonymous proxies
+	 * 
+	 * 3. All Competitor Data was pulled for each 
+	 * region according to each mmy configuration, using 
+	 * detection avoidance, anonymous proxies. All 
+	 * authentication mechanisms were bypassed.
+	 * 
+	 * 4. Raw log collection data and XML/JSON parsed 
+	 * collected data was stored in Oracle. In this 
+	 * case MySQL.
+	 */
+	
 }
