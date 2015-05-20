839c04a5431129b648070b75483cb00bdd0f0cf1
Christopher Gu <cgu1@cdnpal.com>
2015-05-19 19:56:49 -0700

    More important notes about the anonymous proxy and reverse engineering
    	 * Note that IB acquired High Gear Media, TheCarConnection.com and other websites
    	 * after the KKR leveraged buy-out because of this:
    	 * http://www.highgearmedia.com/press_releases/truecar-licenses-pricing-data-to-high-gear-media
    	 *
    	 * The previously had no licensing for the data they were stealing.
    	 *
    	 * The joint AutoData/ChromeData project did not include TrueCar data that was taken and used
    	 * by this application and the reports and updates to CarsDirect produced from it.
    	 *
    	 * The reason explained was that HighGear had good SEO, but in reality it wasn't that good,
    	 * They had the license for this or similar data a full year after it was being used by IB
    	 * by anonymous proxy and reverse engineering.
    	 *
    	 * ACQUISITION REF: http://www.highgearmedia.com/blog/internet-brands-acquires-high-gear-media

--- src/main/java/com/carsdirect/cd/feeds/TrueCarProprietaryRegionalPricingData.java
+++ src/main/java/com/carsdirect/cd/feeds/TrueCarProprietaryRegionalPricingData.java
@@ -6,4 +6,32 @@ public class TrueCarProprietaryRegionalPricingData {
 	 * we will basically empty the rival's database here of all 
 	 * car data, including vID codes, trim codes, make/model info,  ect..
 	 */
+	
+	/*
+     * HTTPS:// was mandatory on TrueCar after they executed a security update against
+	 * this type of attack.
+	 */
+	
+	/*
+     * Data requests were limited by the "Go Away" security update where TrueCar
+     * singled out requests by the application and patched their software after it 
+     * had been running for a couple months
+	 */
+	
+	/*
+	 * Note that IB acquired High Gear Media, TheCarConnection.com and other websites
+	 * after the KKR leveraged buy-out because of this:
+	 * http://www.highgearmedia.com/press_releases/truecar-licenses-pricing-data-to-high-gear-media
+	 * 
+	 * The previously had no licensing for the data they were stealing.
+	 * 
+	 * The joint AutoData/ChromeData project did not include TrueCar data that was taken and used
+	 * by this application and the reports and updates to CarsDirect produced from it.
+	 * 
+	 * The reason explained was that HighGear had good SEO, but in reality it wasn't that good,
+	 * They had the license for this or similar data a full year after it was being used by IB 
+	 * by anonymous proxy and reverse engineering.
+	 * 
+	 * ACQUISITION REF: http://www.highgearmedia.com/blog/internet-brands-acquires-high-gear-media
+	 */
 }
--- src/main/java/com/carsdirect/cd/util/IonExploderHttp.java
+++ src/main/java/com/carsdirect/cd/util/IonExploderHttp.java
@@ -7,4 +7,13 @@ package com.carsdirect.cd.util;
  */
 public class IonExploderHttp {
 
+	/*
+	 * headers randomized, gzip, un-compressed and different ua's mixed
+	 * as well as accept types and other criteria picked at random from
+	 * dictionaries.
+	 * 
+	 * HTTPS:// was mandatory on TrueCar after they executed a security update against
+	 * this type of attack.
+	 */
+	
 }
